df5e918158755a72b9900b383723e659efa2ea93
MisterRuby <ruby8700@naver.com>
2022-11-28 21:24:19 +0900

    feat : OAuth 인증 처리를 위한 컬럼 추가
    provider - 인증 플랫폼
    providerId - 인증 플랫폼에 등록된 사용자의 고유 id값

--- src/domain/user/user.entity.ts
+++ src/domain/user/user.entity.ts
@@ -1,9 +1,15 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ length: 100 })
+  provider: string;
+
+  @Column({ length: 100 })
+  providerId: string;
 
   @Column()
   name: string;
