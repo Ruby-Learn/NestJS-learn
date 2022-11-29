7ed41f63132e076561f3dc13f537e77dd45c34fb
MisterRuby <ruby8700@naver.com>
2022-11-29 12:52:19 +0900

    feat : Id 검증을 위한 Validator 추가

---
+++ src/common/common.validator.ts
@@ -0,0 +1,17 @@
+import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+import { InvalidIdException } from './common.exception';
+
+@Injectable()
+export class IdPipe implements PipeTransform {
+  isId(id: number) {
+    return id > 0;
+  }
+
+  transform(id: number, metadata: ArgumentMetadata): number {
+    if (this.isId(id)) {
+      return id;
+    }
+
+    throw new InvalidIdException();
+  }
+}
