1d0a54b1ff5291d1fe0dba160bde2de6a7a3f1af
MisterRuby <ruby8700@naver.com>
2022-12-01 22:06:46 +0900

    chore : Redis 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as redisStore from 'cache-manager-ioredis';
 
 @Module({
-  imports: [],
+  imports: [
+    CacheModule.register({
+      store: redisStore,
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+      ttl: +process.env.REDIS_TTL,
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
